065e3e39a76ef71853bd507c0a631d02009f772f
eunjoo-choi <epcej0020@gmail.com>
2023-03-11 21:00:28 +0900

    프로젝트 세팅
    - 프로젝트 출처: [github](https://github.com/iamchiwon/RxSwift_In_4_Hours)
    - 수정 사항
       - 팟 대신 SPM으로 RxSwift 사용하도록 변경

---
+++ study-RxSwift/study-RxSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  study-RxSwift
+//
+//  Created by 최은주 on 2023/03/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
